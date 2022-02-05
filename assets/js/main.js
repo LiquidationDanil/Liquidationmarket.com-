400b4cf6ec7e13615305304bae92bfa54757c2ad
wergut <35626608+wergut@users.noreply.github.com>
2022-02-05 17:28:07 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,13 +29,36 @@ $(function () {
     });
 
     // Аккордион меню
-
     $('.accordion_header').on('click', function () {
         let orderContent = $(this).closest('.accordion_item').find('.accordion_content');
         orderContent.slideToggle();
         $(this).toggleClass('active');
     });
 
+    // Модалка
+    document.querySelectorAll("[data-btn]").forEach(item => {
+        item.addEventListener('click', function () {
+            document.body.style.overflow = "hidden";
+            let dataValue = this.getAttribute("data-btn");
+            let modal = document.querySelector('.' + dataValue)
+            modal.style.display = 'flex';
+        });
+    });
+    document.querySelectorAll('.modal').forEach(function (item) {
+        item.addEventListener('click', function (e) {
+            if (e.target === this || e.target.classList.contains('close')) {
+                document.body.style.overflow = "visible";
+                this.style.display = "none";
+            }
+        });
+    });
+    $('.modal_btn').on('click', function(){
+        $('.modal').css({'display': 'none'});
+        $('body').css({'overflow' : 'visible'});
+    });
 
 });
+
+
+   
 
